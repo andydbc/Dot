dc6a8ea1e52db897d748d9b39464cec6787e0889
andydbc <a@andydbc.xyz>
2020-01-19 15:16:40 -0500

    update serial

--- src/main.cpp
+++ src/main.cpp
@@ -51,24 +51,8 @@ static const struct luaL_Reg printlib[] = {
   {NULL, NULL}
 };
 
-void enumerate_ports()
-{
-	std::vector<serial::PortInfo> devices_found = serial::list_ports();
-
-	std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
-
-	while (iter != devices_found.end())
-	{
-		serial::PortInfo device = *iter++;
-
-		printf("(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
-			device.hardware_id.c_str());
-	}
-}
-
 int main()
 {
-	enumerate_ports();
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
