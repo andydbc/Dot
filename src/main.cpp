a3bfd5131cc04916fd44b0ce8ece7a7765bffbf2
andydbc <a@andydbc.xyz>
2020-01-19 12:12:33 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+int keyPos = 0;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+	else
+	{
+		if (action == GLFW_PRESS)
+		{
+			ImGuiTextBuffer* log;
+			log = (ImGuiTextBuffer*)glfwGetWindowUserPointer(window);
+
+			if(key == GLFW_KEY_SPACE)
+				log->appendf("\n");
+			else
+				log->appendf(glfwGetKeyName(key, scancode));
+		}
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+const uint32_t width = 800;
+const uint32_t height = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, false);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "Dot", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+	//glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Load function pointers from GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    const char* glsl_version = "#version 460";
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImFont* pFont = io.Fonts->AddFontFromFileTTF("resources/input_mono_regular.ttf", 18.0f);
+
+	io.ConfigInputTextCursorBlink = true;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; 
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.Colors[ImGuiCol_WindowBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+	style.Colors[ImGuiCol_ChildBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+	style.Colors[ImGuiCol_FrameBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+	style.WindowRounding = 0;
+	style.WindowBorderSize = 0;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	ImGuiTextBuffer log;
+	glfwSetWindowUserPointer(window, &log);
+
+	static char text[1024 * 16] = "def flip_the_dots(x, y, time): \n"
+		"	pass";
+	
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+	
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always, ImVec2(0, 0));
+			ImGui::SetNextWindowSize(ImVec2(400, 600), ImGuiCond_Always);
+            ImGui::Begin("TextEdit", NULL, ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoCollapse);
+			
+			ImGui::InputTextMultiline("", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, 450), ImGuiInputTextFlags_AllowTabInput);
+			ImGui::End();
+        }
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Clean-up imgui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+       
+    // Clean-up GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
+	return 0;
+}
+
