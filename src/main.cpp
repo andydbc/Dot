1ce1480efb1d3427277cf0fe864c25cd6d8edb02
andydbc <a@andydbc.xyz>
2020-01-26 21:25:15 -0500

    syntax error

--- src/main.cpp
+++ src/main.cpp
@@ -209,7 +209,7 @@ void error_callback(int error, const char* description)
 	fprintf(stderr, "Error: %s\n", description);
 }
 
-void send_msg(serial::Serial& &serial)
+void send_msg(serial::Serial& serial)
 {
 #ifndef _WIN32
 	
